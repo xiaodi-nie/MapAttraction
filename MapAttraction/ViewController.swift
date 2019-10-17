3efda2fb70a6f572bf70947ce9b1d23ac9d5be51
yl543 <yl543@duke.edu>
2019-10-17 15:57:42 -0400

    init commit

---
+++ MapAttraction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapAttraction
+//
+//  Created by yuqiao liang on 10/17/19.
+//  Copyright © 2019 yuqiao liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
